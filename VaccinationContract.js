f0999e79031dd7268a728965de3f9cf29fdf0d87
Mihir Wadekar <mwadekar2000@gmail.com>
2018-05-23 10:37:09 +0530

    Add files via upload

---
+++ VaccinationContract.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var VaccinationContract = function () {
+      LocalContractStorage.defineMapProperty(this, "userVaccinesMap");
+}
+
+VaccinationContract.prototype = {
+    init: function () {
+    },
+    setVaccines: function (vaccineArray) {
+        //Set Data
+        var user = Blockchain.transaction.from;
+        this.userVaccinesMap.set(user, vaccineArray);
+
+    },
+    getVaccines: function () {
+        //Get Data
+        var user = Blockchain.transaction.from;
+        return this.userVaccinesMap.get(user);
+    },
+    getMap: function() {
+      return this.userVaccinesMap;
+    }
+};
+
+module.exports =VaccinationContract;
